fccd3b47fdf6c3e0c4e556820cbf2f1df89e618d
tangoyankee <miller.tim108@gmail.com>
2026-04-13 16:05:34 -0400

    configure pmtiles

---
+++ layers/subway-station-ada.js
@@ -0,0 +1,22 @@
+import VectorTile from "ol/layer/VectorTile";
+import { PMTilesVectorSource } from "ol-pmtiles";
+import { Circle, Fill, Stroke, Style } from "ol/style";
+
+export const subwayStationAdaLayer = new VectorTile({
+  source: new PMTilesVectorSource({
+    url: "/demo-pmtiles-openlayers/layer-sources/2024_aug_22_subway_ada.pmtiles",
+    attributions: `<a href="https://data.ny.gov/">NYS</a>; `,
+  }),
+  style: new Style({
+    image: new Circle({
+      radius: 1,
+      fill: new Fill({
+        color: "rgba(255, 0, 0, 1)",
+      }),
+      stroke: new Stroke({
+        color: "gray",
+        width: 1,
+      }),
+    }),
+  })
+})
--- main.js
+++ main.js
@@ -2,13 +2,15 @@ import './style.css';
 import {Map, View} from 'ol';
 import TileLayer from 'ol/layer/Tile';
 import OSM from 'ol/source/OSM';
+import { subwayStationAdaLayer } from './layers/subway-station-ada';
 
 const map = new Map({
   target: 'map',
   layers: [
     new TileLayer({
       source: new OSM()
-    })
+    }),
+    subwayStationAdaLayer,
   ],
   view: new View({
     center: [0, 0],
