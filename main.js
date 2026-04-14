c6365d29fb6730898b55cef23b279950d8155da4
tangoyankee <miller.tim108@gmail.com>
2026-04-14 19:20:09 -0400

    create dynamic content panel

--- main.js
+++ main.js
@@ -1,23 +1,20 @@
-import './style.css';
-import {Map, View} from 'ol';
-import { useGeographic } from 'ol/proj';
-import { stationPointLayer } from './layers/station-point';
-import { stationEnvelopeLayer } from './layers/station-envelope';
-import { nycBaseLayer } from './layers/nyc-base';
+import "./style.css";
+import { Map, View } from "ol";
+import { useGeographic } from "ol/proj";
+import { stationPointLayer } from "./layers/station-point";
+import { stationEnvelopeLayer } from "./layers/station-envelope";
+import { nycBaseLayer } from "./layers/nyc-base";
 
 useGeographic();
 
 const map = new Map({
-  target: 'map',
-  layers: [
-    nycBaseLayer,
-    stationEnvelopeLayer,
-    stationPointLayer,
-  ],
+  target: "map",
+  layers: [nycBaseLayer, stationEnvelopeLayer, stationPointLayer],
   view: new View({
     maxZoom: 16,
     center: [-74, 40.7],
     zoom: 11,
     extent: [-75, 40.2, -73, 41.2],
-  })
+  }),
 });
+
