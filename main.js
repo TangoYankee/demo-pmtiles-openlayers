779111678ffb0a2d4519e4f5c75e6576e65e7e8a
Timothy Miller <miller.tim108@gmail.com>
2026-04-14 14:59:35 -0400

    use pmtiles layer as basemap

--- main.js
+++ main.js
@@ -1,23 +1,21 @@
 import './style.css';
 import {Map, View} from 'ol';
-import TileLayer from 'ol/layer/Tile';
-import OSM from 'ol/source/OSM';
 import { useGeographic } from 'ol/proj';
 import { stationPointLayer } from './layers/station-point';
 import { stationEnvelopeLayer } from './layers/station-envelope';
+import { nycBaseLayer } from './layers/nyc-base';
 
 useGeographic();
 
 const map = new Map({
   target: 'map',
   layers: [
-    new TileLayer({
-      source: new OSM()
-    }),
+    nycBaseLayer,
     stationEnvelopeLayer,
     stationPointLayer,
   ],
   view: new View({
+    maxZoom: 16,
     center: [-74, 40.7],
     zoom: 11,
     extent: [-75, 40.2, -73, 41.2],
